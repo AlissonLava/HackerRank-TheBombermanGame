9f34b6de4067fb7131e5d40dbdc5ef741879de20
AlissonLava <61854869+AlissonLava@users.noreply.github.com>
2023-02-16 17:49:02 -0300

    1st version, works at 3 steps

--- TheBombermanGame/Program.cs
+++ TheBombermanGame/Program.cs
@@ -26,7 +26,7 @@ class Result
         List<string> fullgrid = new List<string>();
         for(int i = 0; i < lin;i++)
         {
-            fullgrid.Add(new string('0', col));
+            fullgrid.Add(new string('O', col));
         }               
         
         if (n <= 3)
@@ -35,24 +35,24 @@ class Result
             {
                 for(int c = 0; c < col; c++) //cycle through columns/chars
                 {
-                    if (grid[l].Substring(c,1) == "0") //finds bomb
+                    if (grid[l].Substring(c,1) == "O") //finds bomb
                     {
                         //PREVIOUS LINE (l-1)
                         
                         if (l > 0) //if not the first line
                         {
                             string pLeft = "", pRight = "";
-                                        //1 away from border
-                            if (c > 0) { pLeft = fullgrid[l-1].Substring(0, c - 1); }
-                            if (c < col - 1) { pRight = fullgrid[l-1].Substring(c + 1, col - c - 1); }
+                                                //1 away from border
+                            if (c > 0)          { pLeft = fullgrid[l-1].Substring(0, c); }
+                            if (c < col - 1)    { pRight = fullgrid[l-1].Substring(c + 1, col - c - 1); }
 
                             fullgrid[l - 1] = pLeft + "." + pRight;
                         }
                         //CURRENT LINE (l)
 
                         string left="", right="", center;
-                                        //2 away from border
-                        if (c > 1)          { left = fullgrid[l].Substring(0, c - 2); }
+                                            //2 away from border
+                        if (c > 1)          { left = fullgrid[l].Substring(0, c - 1); }
                         if (c < col - 2)    { right = fullgrid[l].Substring(c + 2, col - c - 2); }
 
                         if (c == 0 || c == col - 1){ 
@@ -68,8 +68,8 @@ class Result
                         {
                             string nLeft = "", nRight = "";
                                                 //1 away from border
-                            if (c > 0)      { nLeft = fullgrid[l+1].Substring(0, c - 1); }
-                            if (c < col - 1){ nRight = fullgrid[l+1].Substring(c + 1, col - c - 1); }
+                            if (c > 0)          { nLeft = fullgrid[l+1].Substring(0, c); }
+                            if (c < col - 1)    { nRight = fullgrid[l+1].Substring(c + 1, col - c - 1); }
 
                             fullgrid[l + 1] = nLeft + "." + nRight;
                         }
@@ -77,6 +77,7 @@ class Result
                 }
             }
         }
+
         return fullgrid;
     }
 }
